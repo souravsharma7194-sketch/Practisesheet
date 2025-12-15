d8458f16a42c6219fd88489762516a84151386b0
unknown <souravsharma7194@gmail.com>
2025-12-15 22:31:26 +0530

    learnt about callbacks and its issues

---
+++ promises.js
@@ -0,0 +1,51 @@
+// {/*ALL ABOUT PROMISES*/ }
+
+// console.log("namaste");
+
+
+
+// setTimeout(function (){
+// console.log("Javascript");
+// },5000)
+
+// console.log("Season 2");
+
+
+
+
+// const cart = ["shoes","pants","kurta"];
+
+
+// {first we create orderId and then proceed to payment}
+
+// api.createOrder(cart, function(){
+
+
+// api.proceedToPayment(function(){
+
+// api.showOrderSummary(function(){
+// api.UpdateWallet
+// }
+// )
+// })
+
+// })
+
+// this api callback hell or callback structure is also known as PYRAMID OF DOOM.
+
+
+
+// INVERSON OF CONTROL
+
+
+// it means u loose control of your code while managing alot of callbacks.
+
+
+// IMP OF CALLBACKS
+
+//  * callbacks are superpowerful ways to handle asyncoronous operations in js infact ascynchronous programming in js exists because callbacks exists
+
+// ISSUE WITH CALLBACKS
+
+//     * CALLBACK HELL (FUNCTION INSIDE ANOTHER FUNCTION)
+// * INVERSION OF CONTROL (WE GAVE CONTROL OF ONE FUNCTION TO ANOTHER FUNCTION WE DONT KNOW IF THAT FUNC WILL EXECUTE THE NEXT CALLBACK OR NOT SO WE LOOSE CONTROL i.e  INVERSION OF CONTROL)
